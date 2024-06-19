e1d6c108f936ef17296a328d9b5bf6ea03fe474f
Shivansh Chopra <gauravshivansh7@gmail.com>
2024-06-19 18:12:31 +0530

    updating styles

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -15,7 +15,10 @@ const Navbar = ({ setCategory }) => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
       <div className="container-fluid">
-       
+      <a class="navbar-brand" href="#">Check News</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
